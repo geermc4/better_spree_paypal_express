d939ab0d26c755afa7c8290541be2e0ceeb5e90d
geermc4 <geermc4@gmail.com>
2015-07-29 14:52:39 -0700

    use item_total instead of calculating it every time

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -113,12 +113,7 @@ module Spree
       # For example, if shippng costs $10, and is free with a promotion, shipment_sum is now $10
       shipment_sum = current_order.shipments.map(&:discounted_cost).sum
 
-      # This calculates the item sum based upon what is in the order total, but not for shipping
-      # or tax.  This is the easiest way to determine what the items should cost, as that
-      # functionality doesn't currently exist in Spree core
-      item_sum = current_order.total - shipment_sum - current_order.additional_tax_total
-
-      if item_sum.zero?
+      if current_order.item_total.zero?
         # Paypal does not support no items or a zero dollar ItemTotal
         # This results in the order summary being simply "Current purchase"
         {
@@ -135,7 +130,7 @@ module Spree
           },
           :ItemTotal => {
             :currencyID => current_order.currency,
-            :value => item_sum
+            :value => current_order.item_total
           },
           :ShippingTotal => {
             :currencyID => current_order.currency,
